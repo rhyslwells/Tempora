b1eeac178b4677855e93e560974b03e312e88927
Rhys Wells <rhyslwells@outlook.com>
2025-11-05 19:10:33 +0000

    requirements updated

--- notebooks/For ML_Tools/Correlation.py
+++
@@ -1,6 +0,0 @@
-from sklearn.preprocessing import StandardScaler
-import pandas as pd, seaborn as sns, matplotlib.pyplot as plt
-
-scaled = pd.DataFrame(StandardScaler().fit_transform(df), 
-                      index=df.index, columns=df.columns)
-sns.heatmap(scaled.corr(), cmap='coolwarm', center=0)
